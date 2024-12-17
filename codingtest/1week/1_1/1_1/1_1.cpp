e8aff7a65ee458a1ce58b8e8d7c78a80df1ab5ef
bhsohn <hanmuson1@gmail.com>
2024-12-18 00:12:38 +0900

    test

--- codingtest/1week/1_1/1_1/1_1.cpp
+++ codingtest/1week/1_1/1_1/1_1.cpp
@@ -9,11 +9,14 @@ int n;
 int main() {
     cin >> n;
     int a = 0;
+    int cnt = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i; j++) {
             a += i + j;
+            cnt++;
         }
     }
     cout << a << '\n';
+    cout << "cnt:" << cnt << '\n';
     return 0;
 }
